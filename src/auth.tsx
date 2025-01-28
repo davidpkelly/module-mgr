a5fabeb5841570cb8825881e75a8aee0a9a83b88
David Kelly <dvkelly@juniper.net>
2025-01-27 18:11:22 -0800

    Call amplify auth

--- src/auth.tsx
+++ src/auth.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
-
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
+import { getCurrentUser, signIn, signOut } from "aws-amplify/auth";
 
 export interface AuthContext {
   isAuthenticated: boolean;
@@ -13,48 +10,52 @@ export interface AuthContext {
 
 const AuthContext = React.createContext<AuthContext | null>(null);
 
-const key = 'tanstack.auth.user'
+// const key = "mm.auth.user";
 
-function getStoredUser() {
-  return localStorage.getItem(key)
-}
+// function getStoredUser() {
+//   return localStorage.getItem(key);
+// }
 
-function setStoredUser(user: string | null) {
-  if (user) {
-    localStorage.setItem(key, user)
-  } else {
-    localStorage.removeItem(key)
-  }
-}
+// function setStoredUser(user: string | null) {
+//   if (user) {
+//     localStorage.setItem(key, user);
+//   } else {
+//     localStorage.removeItem(key);
+//   }
+// }
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = React.useState<string | null>(getStoredUser())
-  const isAuthenticated = !!user
+  // const [user, setUser] = React.useState<string | null>(getStoredUser());
+  const [user, setUser] = React.useState<string | null>(null);
+  const isAuthenticated = !!user;
 
   const logout = React.useCallback(async () => {
-    await sleep(250)
-    setStoredUser(null)
-    setUser(null)
-    await sleep(250)
-  }, [])
+    await signOut();
+    // setStoredUser(null);
+    setUser(null);
+  }, []);
 
-  const login = React.useCallback(async (username: string, password: string) => {
-    await sleep(500)
-
-    setStoredUser(username)
-    setUser(username)
-    console.log('pw: ', password);
-  }, [])
+  const login = React.useCallback(
+    async (username: string, password: string) => {
+      await signIn({ username, password });
+      // setStoredUser(username);
+      setUser(username);
+    },
+    []
+  );
 
   React.useEffect(() => {
-    setUser(getStoredUser())
-  }, [])
+    getCurrentUser().then(({ username, userId, signInDetails }) => {
+      console.log("[getCurrentUser]", { username, userId, signInDetails });
+      setUser(username);
+    });
+  }, []);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 export function useAuth() {
