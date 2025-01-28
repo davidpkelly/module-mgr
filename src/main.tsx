c68597e2df8b3c407a2104db4849f1abe002c87d
David Kelly <dvkelly@juniper.net>
2025-01-27 20:00:15 -0800

    Call amplify configure

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.tsx";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
+
+Amplify.configure(outputs);
 
 const rootElement = document.getElementById("root")!;
 
